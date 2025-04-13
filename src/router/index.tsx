1a0bada93845b2f77cd509c4bad31daa90e78abe
cleopatraropotgitprojects <cleopatra.gitprojects@gmail.com>
2025-04-13 13:40:43 +0300

    KAN-3 (#2)

---
+++ src/router/index.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import AdminLayout from "../layouts/adminLayout";
+import DashboardPage from "../pages/dashboard/dashboard";
+import UsersPage from "../pages/users/users";
+import SettingsPage from "../pages/settings/settings";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <Navigate to="/dashboard" />,
+            },
+            {
+                path: 'dashboard',
+                element: <DashboardPage />,
+            },
+            {
+                path: 'users',
+                element: <UsersPage />,
+            },
+            {
+                path: 'settings',
+                element: <SettingsPage />,
+            },
+        ],
+    },
+]);

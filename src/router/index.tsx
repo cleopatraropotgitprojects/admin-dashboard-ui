feda67ee9ed2835b09b0cf68912c2ea9c98512af
cleopatraropot <cleopatraropot@gmail.com>
2025-04-13 15:29:22 +0300

    Simple login system

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,28 +3,26 @@ import AdminLayout from "../layouts/adminLayout";
 import DashboardPage from "../pages/dashboard/dashboard";
 import UsersPage from "../pages/users/users";
 import SettingsPage from "../pages/settings/settings";
+import LoginPage from "@/pages/auth/loginPage";
+import ProtectedRoute from "@/router/protectedRoute";
 
 export const router = createBrowserRouter([
     {
+        path: '/login',
+        element: <LoginPage />,
+    },
+    {
         path: '/',
-        element: <AdminLayout />,
+        element: (
+            <ProtectedRoute>
+                <AdminLayout />
+            </ProtectedRoute>
+        ),
         children: [
-            {
-                index: true,
-                element: <Navigate to="/dashboard" />,
-            },
-            {
-                path: 'dashboard',
-                element: <DashboardPage />,
-            },
-            {
-                path: 'users',
-                element: <UsersPage />,
-            },
-            {
-                path: 'settings',
-                element: <SettingsPage />,
-            },
+            { index: true, element: <Navigate to="/dashboard" /> },
+            { path: 'dashboard', element: <DashboardPage /> },
+            { path: 'users', element: <UsersPage /> },
+            { path: 'settings', element: <SettingsPage /> },
         ],
     },
 ]);
